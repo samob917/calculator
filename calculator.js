8450542bb12a220df604f35425ae898c3dc967d9
Sam Oberly <samob917@outlook.com>
2025-01-06 20:50:19 -0500

    Create and test multiplication function
    Multiplication function works as intended according to the
    console.log tests. Negatives and positives are handled as expected.
    Now finishing up with division.

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 function add(a,b) {
     return a+b;
-}
+};
 console.log("Addition tests");
 console.log(`${add(1, 9)} should return 10`);
 console.log(`${add(-5, 12)} should be 7`);
@@ -8,11 +8,18 @@ console.log(`${add(5,-1)} should be 4`);
 
 function subtract(a,b) {
     return a-b;
-}
+};
 console.log("Subtraction tests");
 console.log(`${subtract(100, 25)} should return 75`);
 console.log(`${subtract(-23, -5)} should return -18`);
 console.log(`${subtract(27, -3)} should return 30`);
 
-function multiply(a,b) {}
+function multiply(a,b) {
+    return a*b;
+};
+console.log("Multiplication tests");
+console.log(`${multiply(21, 21)} should return 441`);
+console.log(`${multiply(-3, 7)} should return -21`);
+console.log(`${multiply(-9, -8)} should return 72`);
+
 function divide(a,b) {}
