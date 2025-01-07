90ce79592c28623e2c0c533fa1c1c77be18616a5
Sam Oberly <samob917@outlook.com>
2025-01-06 20:39:21 -0500

    Create add function
    The add function is one of the four key functions. I created
    3 simple tests in the console to ensure that add works. This
    implementation passes all the tests. Now moving to subtraction.

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function add(a,b) {
+    return a+b;
+}
+
+console.log(`${add(1, 9)} should return 10`);
+console.log(`${add(-5, 12)} should be 7`);
+console.log(`${add(5,-1)} should be 4`);
+
+function subtract(a,b) {
+    return a-b;
+}
+console.log(`${subtract(100, 25)} should return 75`)
+function multiply(a,b) {}
+function divide(a,b) {}
