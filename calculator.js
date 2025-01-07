c949c7a714ef1111ca718b84f13df3109a0ab757
Sam Oberly <samob917@outlook.com>
2025-01-06 20:56:58 -0500

    Create and test divide function
    Division function passes the console.log tests and handles
    divide by 0 functions by returning a message that says division by 0
    can not be done. Now moving on to the next step.

--- calculator.js
+++ calculator.js
@@ -22,4 +22,14 @@ console.log(`${multiply(21, 21)} should return 441`);
 console.log(`${multiply(-3, 7)} should return -21`);
 console.log(`${multiply(-9, -8)} should return 72`);
 
-function divide(a,b) {}
+function divide(a,b) {
+    if (b === 0) {
+        return "NO DIVIDING BY 0 LOSER.";
+    } else {
+        return a/b;
+    };
+};
+console.log("Division tests");
+console.log(`${divide(6, 2)} should return 3`);
+console.log(`${divide(-81,5)} should return -16.2`);
+console.log(`${divide(12, 0)} should return "NO DIVIDING BY 0 LOSER."`);
